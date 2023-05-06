cc1976671633de3a93e1d55189330073f2b96a7f
SpearsGoode <spearsgoode@protonmail.com>
2023-05-06 16:50:46 -0700

    init

---
+++ src/main/java/com/spearsgoode/site/SpearsGoodeWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.spearsgoode.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpearsGoodeWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpearsGoodeWebsiteApplication.class, args);
+	}
+
+}
