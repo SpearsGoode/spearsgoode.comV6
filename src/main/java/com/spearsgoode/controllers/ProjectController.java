45b9a609fc21f1361358efbaca0e2f54a6eaea2f
SpearsGoode <spearsgoode@protonmail.com>
2023-05-08 23:21:35 -0700

    add navigation and a few thymeleaf fragments to project page - fragments must be updated because the current ussage is depreciated

--- src/main/java/com/spearsgoode/controllers/ProjectController.java
+++ src/main/java/com/spearsgoode/controllers/ProjectController.java
@@ -1,4 +1,16 @@
 package com.spearsgoode.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ProjectController {
+
+    @GetMapping("/projects")
+    public String home(Model model) {
+
+        // Return the view name
+        return "projects";
+    }
 }
