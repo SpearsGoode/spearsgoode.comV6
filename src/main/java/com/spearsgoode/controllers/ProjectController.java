3df1b48e61539f187df8f59672cfecca212080cc
SpearsGoode <spearsgoode@protonmail.com>
2023-06-18 12:03:52 -0700

    prep for art page implementation

--- src/main/java/com/spearsgoode/controllers/ProjectController.java
+++ src/main/java/com/spearsgoode/controllers/ProjectController.java
@@ -84,7 +84,7 @@ add entry to DB:
   Abstract-Artwork.com
     curl http://localhost:8080/projects/add -d title=Abstract-Artwork.com -d tag=abstract -d "date=Completed: April 2019" -d img=abstract-artwork.jpg -d "alt=Abstract-Artwork.com home page screenshot" -d link=https://archive.spearsgoode.com/abstract-artwork.com -d "intro=Haven for Abstract Artists. View abstract artwork from around the globe, meet other abstract artists, and find out what inspires them." -d info=abstract
   Armada DAO: FIXME
-    curl http://localhost:8080/projects/add -d "title=Armada DAO | Ape Armada" -d tag=armada -d "date=Discontinued: October 2022" -d img=ape_soldier.jpg -d "alt=Ape Armada Ape Soldier" -d link=null -d "intro=A Play to Earn, Multiplayer, First Person Shooter designed to tackle significant problems concerning the design of current generation Play to Earn (P2E) games" -d info=armada
+    curl http://localhost:8080/projects/add -d "title=Ape Armada DAO" -d tag=armada -d "date=Discontinued: October 2022" -d img=ape_soldier.jpg -d "alt=Ape Armada Ape Soldier" -d link=null -d "intro=A Play to Earn, Multiplayer, First Person Shooter designed to tackle significant problems concerning the design of current generation Play to Earn (P2E) games" -d info=armada
   aHODLm: FIXME
     curl http://localhost:8080/projects/add -d "title=aHODLm Indicator" -d tag=aHODLm -d "date=Shelved: August 2020" -d img=aHODLm.jpg -d "alt=aHODLm indicator BTC example" -d link=null -d "intro=Detects likelihood of significant movements. At points of high risk it takes profit and at points of low risk it increases the position size. Since its inception it has proven its ability to swing trade effectively with minimal oversight." -d info=aHODLm
   SpearsGoode.com:
---
+++ src/main/java/com/spearsgoode/models/Art.java
@@ -0,0 +1,106 @@
+package com.spearsgoode.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Art {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String title;
+    private String medium;
+    private String link;
+    private String type;
+    private String category;
+    private double height;
+    private double width;
+
+    /* ----------
+    CONSTRUCTORS
+    ---------- */
+
+    public Art() {
+    }
+
+    public Art(String title, String medium, String link, String type, String category, double height, double width) {
+        this.title = title;
+        this.medium = medium;
+        this.link = link;
+        this.type = type;
+        this.category = category;
+        this.height = height;
+        this.width = width;
+    }
+
+    /* ----------
+    GETTERS
+    ---------- */
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getMedium() {
+        return medium;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    /* ----------
+    SETTERS
+    ---------- */
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setMedium(String medium) {
+        this.medium = medium;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+}
