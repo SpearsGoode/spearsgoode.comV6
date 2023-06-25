ed66c3b648bae541e200ba6b9414157325cbe5a4
SpearsGoode <spearsgoode@protonmail.com>
2023-06-24 17:04:34 -0700

    js work around working on all sections - may need more improvments later

--- src/main/resources/static/js/mason.js
+++ src/main/resources/static/js/mason.js
@@ -10,12 +10,57 @@
 //      msnry.layout();
 //    });
 
-  const msnry = new Masonry('#mason', {
-    "percentPosition": true,
-    "itemSelector": '.mason-item',
-    "initLayout": false
-  });
-  const Col = document.getElementById('collapseLogo');
-  Col.addEventListener('shown.bs.collapse', function() {
-      msnry.layout();
-  });
+//LOGO
+    const msnryLogo = new Masonry('#masonLogo', {
+        "percentPosition": true,
+        "itemSelector": '.mason-item',
+        "initLayout": false
+    });
+    const collLogo = document.getElementById('collapseLogo');
+    collLogo.addEventListener('shown.bs.collapse', function() {
+        msnryLogo.layout();
+    });
+
+//Cover
+    const msnryCover = new Masonry('#masonCover', {
+        "percentPosition": true,
+        "itemSelector": '.mason-item',
+        "initLayout": false
+    });
+    const collCover = document.getElementById('collapseCover');
+    collCover.addEventListener('shown.bs.collapse', function() {
+        msnryCover.layout();
+    });
+
+//Paint
+    const msnryPaint = new Masonry('#masonPaint', {
+            "percentPosition": true,
+            "itemSelector": '.mason-item',
+            "initLayout": false
+    });
+    const collPaint = document.getElementById('collapsePaint');
+    collPaint.addEventListener('shown.bs.collapse', function() {
+        msnryPaint.layout();
+    });
+
+//Draw
+    const msnryDraw = new Masonry('#masonDraw', {
+            "percentPosition": true,
+            "itemSelector": '.mason-item',
+            "initLayout": false
+    });
+    const collDraw = document.getElementById('collapseDraw');
+    collDraw.addEventListener('shown.bs.collapse', function() {
+        msnryDraw.layout();
+    });
+
+//Other
+    const msnryOther = new Masonry('#masonOther', {
+            "percentPosition": true,
+            "itemSelector": '.mason-item',
+            "initLayout": false
+    });
+    const collOther = document.getElementById('collapseOther');
+    collOther.addEventListener('shown.bs.collapse', function() {
+        msnryOther.layout();
+    });
